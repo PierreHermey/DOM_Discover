834e901c45c0f2b4aaaa955fe7bf6ea9be415cf3
3p3h97 <pierre.hermey@laposte.net>
2018-03-27 14:41:40 +0200

    Commit Pierre TextGrow + BoutonHeight JS

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
-
-function changeHeight() {
-	var rect = document.getElementById('rectangle'); 
-  if ( rect.clientHeight < 300 ){
-   	console.log(rect.clientHeight);
-    rect.style.height =	(rect.clientHeight + 10) + "px";
-  console.log(rect.clientHeight + 10);
+function toggleSize(){
+  var mySizeClasses = document.getElementById("btn").classList;
+  if (mySizeClasses.contains("textNormal")) {
+  mySizeClasses.remove("textNormal");
+  mySizeClasses.add("textGrow");
+  } else {
+  mySizeClasses.add("textNormal");
+  mySizeClasses.remove("textGrow");
   }
-   if ( rect.clientHeight >= 300 ){
-   	rect.style.height = "100px";
-  	
-   }
- 
-}
+  }
+  
+  function changeHeight() {
+  var rect = document.getElementById('rectangle'); 
+  if ( rect.clientHeight < 300 ){
+  rect.style.height =	(rect.clientHeight + 10) + "px";
+  }
+  if ( rect.clientHeight >= 300 ){
+  rect.style.height = "100px";
+  
+  }
+  }
