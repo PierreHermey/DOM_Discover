3a805b156a2bdf9fcf0ac09307029ca057cb5541
Trelanor <jonathan.n@codeur.online>
2018-03-28 15:40:17 +0200

    update

--- script.js
+++ script.js
@@ -63,16 +63,3 @@ document.body.addEventListener('keyup', function(event) {
   if(event.keyCode == 38) 
     rect.style.top="600px"; 
 })
-
-document.body.addEventListener('keydown', function(event) {
-
- if(event.keyCode == 40 )
-    rect.style.bottom="500px"; 
-})
-
-
-document.body.addEventListener('keyup', function(event) {
-
-  if(event.keyCode == 40) 
-    rect.style.bottom="400px"; 
-}) 
