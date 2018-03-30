b9a3e83b014217cca6200b7886f48ca7d46ecc66
Trelanor <jonathan.n@codeur.online>
2018-03-30 15:37:12 +0200

    upadate

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-var rect = document.getElementById('rectangle')
-var classes = rect.classList
+var rect = document.getElementById('rectangle');
+var classes = rect.classList;
 
 
 function toggleSize(){
@@ -50,12 +50,12 @@ function disappear(){
  } 
 }
 
-var move = document.body.addEventListener;
+//var move = document.body.addEventListener();
 var zone = document.getElementById("zone");
 
 
 
-move('keydown',function(event){
+document.body.addEventListener('keydown',function(event){
 //Movement
 var moveLeft = (rect.style.left.replace('px','')*1)-10 + "px";
 var moveRight = (rect.style.left.replace('px','')*1)+10 + "px";
@@ -65,37 +65,48 @@ var moveDown = (rect.style.top.replace('px','')*1)+10 + "px";
 var moveLeftStop = (rect.style.left.replace('px','')*1)+0+ "px";
 var moveUpStop = (rect.style.top.replace('px','')*1)+0 + "px";
 //Offset Object
-var goLeft = (rect.offsetLeft)
-var goTop = (rect.offsetTop)
+var goLeft = (rect.offsetLeft);
+var goTop = (rect.offsetTop);
 //Logs
-console.log(moveRight)
-console.log(goLeft)
+console.log(moveRight);
+console.log(goLeft);
+console.log(rect);
+console.log(zone);
 
 //BOUTON GAUCHE
 
 if (event.keyCode==37 ){
-rect.style.left=moveLeft
+rect.style.left=moveLeft;
 }
 if (goLeft<10){
-rect.style.left=moveLeftStop
+rect.style.left=moveLeftStop;
 }
+
 //BOUTON DROIT
 if (event.keyCode==39){
-rect.style.left=moveRight
+rect.style.left=moveRight;
+ if (goLeft>190){
+ rect.style.left=moveLeftStop;
+
+ }
+ 
 }
 
 // BOUTON HAUT
 
 if (event.keyCode==38){
-rect.style.top=moveUp
+rect.style.top=moveUp;
 }
 if (goTop<10){
-rect.style.top=moveUpStop
+rect.style.top=moveUpStop;
 }
 
 // BOUTON BAS
 if (event.keyCode==40){
-rect.style.top=moveDown
+rect.style.top=moveDown;
+if (goTop>190){
+ rect.style.top=moveUpStop;
+}
 }
 
 
